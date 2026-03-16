4f619952f5987cf66544d0ef76b5c5e00cfcced6
Yehouda Cohen <coheny748@gmail.com>
2026-03-16 18:19:31 +0200

    add route * to Navigate to HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from 'react-router'
+import { BrowserRouter,Routes,Route, Navigate } from 'react-router'
 
 import './App.css'
 import HomePage from './pages/HomePage'
@@ -14,9 +14,13 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path='/' element={<HomePage/>}/>
+
       <Route path='/add' element={<AddlauncherPage/>}/>
       <Route path='/launcher/:id/edit' element={<AddlauncherPage/>}/>
+
       <Route path='/launcher/:id' element={<LauncherDetailsPage/>}/>
+
+      <Route path='*' element={<Navigate to='/'/>}/>
     </Routes>
     </BrowserRouter>
   )
