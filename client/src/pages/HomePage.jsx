2229446c8e512c2397c75a6cbc7f42ac155d2eac
Yehouda Cohen <coheny748@gmail.com>
2026-03-16 12:47:41 +0200

    add react_router

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
 import { useState ,useEffect } from "react";
+import { getAllLaunchers } from "../services/launcherService";
 
 
+function HomePage(){
+    const[launchers,setLaunchers]=useState([])
+    const[search,setSearch]=useState('')
+    const[filter,setFilter]=useState('')
+
+    useEffect(()=>{
+        getAllLaunchers().then(setLaunchers)
+    },[])
+}
